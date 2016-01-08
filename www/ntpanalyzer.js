9245e2d56cab9d63058a8dc66d837721a41adda8
Hendrik Geßner <HENGE01@gmail.com>
2016-01-08 14:45:43 +0100

    Removed channel.waitForInitialization() because it blocks the initialization process.

--- www/ntpanalyzer.js
+++ www/ntpanalyzer.js
@@ -1,18 +1,13 @@
 var argscheck = require('cordova/argscheck'),
-    channel = require('cordova/channel'),
     exec = require('cordova/exec'),
     cordova = require('cordova');
-
-channel.createSticky('onCordovaInfoReady');
-// Tell cordova channel to wait on the CordovaInfoReady event
-channel.waitForInitialization('onCordovaInfoReady');
 
 function Analyzer() {
 }
 
 Analyzer.prototype.diagnose = function(host, successCallback, errorCallback) {
-	argscheck.checkArgs('SFF', 'NtpAnalyzer.diagnose', arguments);
-	exec(successCallback, errorCallback, "NtpAnalyzer", "diagnose", [host]);
+    argscheck.checkArgs('SFF', 'NtpAnalyzer.diagnose', arguments);
+    exec(successCallback, errorCallback, "NtpAnalyzer", "diagnose", [host]);
 };
 
 module.exports = new Analyzer();
