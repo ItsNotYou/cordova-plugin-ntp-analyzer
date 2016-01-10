8efd224fe80fca7f622c5797440ec9efb1db812d
Hendrik Geßner <HENGE01@gmail.com>
2016-01-10 19:25:59 +0100

    Replaced jQuery dependency with native code.

--- src/browser/NtpAnalyzerProxy.js
+++ src/browser/NtpAnalyzerProxy.js
@@ -2,12 +2,27 @@ var browser = require('cordova/platform');
 var cordova = require('cordova');
 var $ = require('jquery');
 
+function ajax(options) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+            if(xmlhttp.status >= 200 && xmlhttp.status <= 299){
+                options.success(xmlhttp.responseText);
+            } else {
+                options.error(xmlhttp);
+            }
+        }
+    };
+
+    xmlhttp.open("GET", options.url, true);
+    xmlhttp.send();
+}
+
 function diagnose(host, success, error) {
-    $.ajax({
+    ajax({
         url: "http://localhost:8080/ntpanalyzer/rest/diagnose?host=" + encodeURIComponent(host),
         success: function(result) { success(parseInt(result)); },
-        error: error,
-        crossDomain: true
+        error: error
     });
 }
 
